25e35c1ad8d48abd15a6cae4cd56fc09f5d5123a
qdtrl <quentindtouroul@gmail.com>
2021-04-06 09:44:35 +0200

    footer

--- components/Layout/Footer/index.jsx
+++ components/Layout/Footer/index.jsx
@@ -8,9 +8,9 @@ const Footer = () => {
        <section>
           <div className={styles.descriptions}>
             <Link href="/">                
-              <a className={styles.logo}>JC <span>Sailing</span></a>
+              <a className={styles.logo}><span>JC</span> Sailing</a>
             </Link>
-            <p><span>JC</span> Sailing est une entreprise spécialisée dans la réparation de bateaux</p>
+            <p><span>JC</span> Sailing est une entreprise spécialisée dans la vente et réparation de bateaux de plaisance</p>
           </div>
           <div className={styles.list_links}>
             <h3>Liens utiles</h3>
@@ -27,8 +27,14 @@ const Footer = () => {
               <a className={styles.navbar_links}>Contact</a>
             </Link>      
           </div>
+          <address className={styles.details}>
+            <h3>Contactez-nous</h3>
+            <p className={styles.address}>Rue des Pommiers <br/> 50400 Saint-Planchers</p>
+            <p>Téléphone: <a href="tel:0620753173">06.20.75.31.73</a> </p>
+            <p>E-mail: <a href="mailto:jcsailing@gmail.com">jcsailing@gmail.com</a> </p>
+          </address>
         </section>
-        <p>© 2021 <span>JC</span> Sailing, tous droits réservés.</p>
+        <p className={styles.mentions}>© 2021 <span>JC</span> Sailing, tous droits réservés.</p>
       </footer>
     </>
   )
