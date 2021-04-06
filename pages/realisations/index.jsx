8eb2e5d96b0bd73619400a7115dd55eabecdbe09
qdtrl <quentindtouroul@gmail.com>
2021-04-06 16:50:32 +0200

    page productions

--- pages/realisations/index.jsx
+++ pages/realisations/index.jsx
@@ -1,14 +1,114 @@
 import Head from "next/head";
+import Image from "next/image";
+import { useState } from "react";
+import Modal from 'react-modal';
+import Moment from 'moment';
 import Layout from "../../components/Layout";
+import { API_URL } from "../../config";
+import styles from './realisations.module.scss';
 
-const Productions = () => {
+export const getStaticProps = async () => {
+  const response = await fetch(`${API_URL}productions`);
+  const productions = await response.json();
+  return {
+    props: { productions },
+  };
+};
+
+const Productions = ({ productions }) => {
+  Moment.locale("fr");
+  const [openModal, setOpenModal] = useState(false)
+  const [productiondata, setProductiondata] = useState([])
+  const [images, setImages] = useState([])
+  const [image, setImage] = useState([])
+
+  const handleClick = (event) => {
+    setOpenModal(true);
+    setProductiondata(productions[Number(event.target.id)].production);
+    setImages(productions[Number(event.target.id)].images);
+    setImage(productions[Number(event.target.id)].images[0]);
+  }
+
+  const handleClickImage = (event) => {
+    setImage(images[Number(event.target.id)]);
+  }
+  const handleRequestCloseFunc = () => {
+    setOpenModal(false);
+  }
+
   return (
     <>
+      <Modal
+        isOpen={openModal}
+        ariaHideApp={false}
+        contentLabel="Card Production"
+        shouldCloseOnEsc={true}
+        shouldCloseOnOverlayClick={true}
+        onRequestClose={handleRequestCloseFunc}
+        style={{
+          overlay: {
+            padding: "0",
+            margin: "0",
+            backgroundColor: "#424040b0",
+            zIndex: "1"
+          },
+          content: {
+            color: "black",
+            marginTop: "80px",
+            height: "80vh",
+            width: "60vw",
+            marginLeft: "18vw"
+          }
+        }}
+      >
+        <section className={styles.modal_content}>
+          <h2>{productiondata.name}</h2>
+          <Image
+            src={`${image}`}
+            alt={productiondata.name}
+            width={400}
+            height={400}
+          />
+          <ul className={styles.carousel_images}>
+            { images.map((link, index) => (
+              <li onClick={handleClickImage} key={index}>
+                <Image
+                  id={index}
+                  src={`${link}`}
+                  alt={productiondata.name}
+                  width={80}
+                  height={80}
+                />
+              </li>
+            ))}
+          </ul>
+          <p className={styles.image_description}>{productiondata.description}</p>
+          <p className={styles.image_date}>Date du projet : {Moment(productiondata.created_at).format('LL')}</p>  
+        </section>
+      </Modal>
       <Head>
         <title>Réalisations</title>
       </Head>
       <Layout>
-        <h1>Réalisations</h1>
+        <section className={styles.realisations}>
+          <h1>Réalisations</h1>
+          <p>Voici certains de nos travaux effectués</p>
+          { productions.length > 0 && (
+            <ul>
+              {productions.map(({ production, images }, index) => (
+                <li onClick={handleClick} key={production.id}>
+                  <Image
+                    id={index}
+                    src={`${images[0]}`}
+                    alt={production.name}
+                    width={300}
+                    height={300}
+                  />
+                </li>
+              ))}
+            </ul>
+          )}
+        </section>
       </Layout>
     </>
   )
