490387c9ae815dbe54ed7251fbbffe427282b681
qdtrl <quentindtouroul@gmail.com>
2021-09-28 09:40:34 +0200

    reponsive suppliers

--- components/Suppliers/index.jsx
+++ components/Suppliers/index.jsx
@@ -1,53 +1,86 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { useState, useEffect } from "react";
 import styles from './suppliers.module.scss';
 
+
 const Suppliers = () => {
+  const [windowHeight, setWindowHeight] = useState(800);
+  const [index, setIndex ] = useState(0);
+
+  useEffect(() => {
+    if (window.innerWidth) {
+      setWindowHeight(window.innerWidth);
+    }
+  }, [])
+  
+  const suppliers = [
+    <Image
+    src="/images/osculati.png"
+    alt="Osculati"
+    height={120}
+    width={120}
+    />,
+    <Image
+    src="/images/boero.png"
+    alt="Boero"
+    height={90}
+    width={90}
+    />,
+    <Image
+    src="/images/soloplast.png"
+    alt="Soloplast"
+    height={50}
+    width={144}
+    />,
+    <Image
+    src="/images/agl_marine.png"
+    alt="Agl Marine"
+    height={60}
+    width={144}
+    />,
+    <Image
+      src="/images/awl_grip.png"
+      alt="Awl Grip"
+      height={40}
+      width={144}
+    />
+  ];
+
+  const handleBack = () => {
+    setIndex(index - 2);
+  }
+
+  const handleNext = () => {
+    setIndex(index + 2);
+  }
   return (
-    <section className={styles.suppliers}>
-      <h2>Nos Fournisseurs</h2>
-      <ul>
-        <li>
-          <Image
-            src="/images/osculati.png"
-            alt="Osculati"
-            height={120}
-            width={120}
-          />
-        </li>
-        <li>
-          <Image
-            src="/images/boero.png"
-            alt="Boero"
-            height={90}
-            width={90}
-          />
-        </li>
-        <li>
-          <Image
-            src="/images/soloplast.png"
-            alt="Soloplast"
-            height={50}
-            width={144}
-          />
-        </li>
-        <li>
-          <Image
-            src="/images/agl_marine.png"
-            alt="Agl Marine"
-            height={60}
-            width={144}
-          />
-        </li>
-        <li>
-          <Image
-            src="/images/awl_grip.png"
-            alt="Awl Grip"
-            height={40}
-            width={144}
-          />
-        </li>
-      </ul>
-    </section>
+    <>
+    { windowHeight >= 1200 ?  
+      <section className={styles.suppliers_web}>
+        <h2>Nos Fournisseurs</h2>
+        <ul>
+          {suppliers.map(supplier => (<li>{supplier}</li>))}
+        </ul>
+      </section> 
+      : 
+      <section className={styles.suppliers_mobile}>
+        <h2>Nos Fournisseurs</h2>
+        { index !== 4 ? 
+          <ul>
+            { index != 0 && <div className={styles.arrow_left} onClick={handleBack}>{`<`}</div>}
+            <li>{suppliers[index]}</li>
+            <li>{suppliers[index + 1]}</li>
+            <div className={styles.arrow_right} onClick={handleNext}>{`>`}</div> 
+          </ul>
+          : 
+          <ul>
+            <div className={styles.arrow_left} onClick={handleBack}>{`<`}</div>
+            <li>{suppliers[index]}</li>
+          </ul>
+        }
+      </section>  }
+    </>
+   
   )
 };
 
