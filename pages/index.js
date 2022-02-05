25739c4300222b40d46285ce791fa420c78702ad
qdtrl <quentindtouroul@gmail.com>
2022-02-05 01:39:29 +0100

    balise meta granville

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
         <title>JC Sailing - Entretien, réparation et vente de voilier bateaux moteur et d'équipement nautique</title>
         <meta name='description' content={`JC Sailing est une entreprise spécialisée dans l'entretien, la vente et la réparation de bateaux de plaisance à voile et moteur et d'équipement nautique`}/>
-        <meta name='keywords' content='jc sailing, jean-christophe théo, entretien, réparation, antifouling, stratification, nettoyage, carénage, peinture, polyester, primaire époxy, calage de bateau, achat, voilier, moteur, bateau, semi-rigide' />
+        <meta name='keywords' content='jc sailing, jean-christophe théo, granville, entretien, réparation, antifouling, stratification, nettoyage, carénage, peinture, polyester, primaire époxy, calage de bateau, achat, voilier, moteur, bateau, semi-rigide' />
         <meta name="google-site-verification" content="eOeLV1_1S32wt2D9FDkCgZkSFLIWwHthE2LhzmfiuEQ" />
       </Head>
       <Layout show={false}>
