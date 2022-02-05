7de405fe9180577675eb29d76d6d1a52ea299767
qdtrl <quentindtouroul@gmail.com>
2022-02-05 01:16:23 +0100

    balise meta google

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ const Home = () => {
         <title>JC Sailing - Entretien, réparation et vente de voilier bateaux moteur et d'équipement nautique</title>
         <meta name='description' content={`JC Sailing est une entreprise spécialisée dans l'entretien, la vente et la réparation de bateaux de plaisance à voile et moteur et d'équipement nautique`}/>
         <meta name='keywords' content='jc sailing, jean-christophe théo, entretien, réparation, antifouling, stratification, nettoyage, carénage, peinture, polyester, primaire époxy, calage de bateau, achat, voilier, moteur, bateau, semi-rigide' />
+        <meta name="google-site-verification" content="eOeLV1_1S32wt2D9FDkCgZkSFLIWwHthE2LhzmfiuEQ" />
       </Head>
       <Layout show={false}>
         <main>
